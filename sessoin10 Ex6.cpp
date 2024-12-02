3d2ea8e58281bacd9daf2cb8a72e7c3955c3bea6
nguyenbabie1 <brokeniuem569@gmail.com>
2024-12-03 02:07:08 +0700

    Update sessoin10 Ex6.cpp

--- sessoin10 Ex6.cpp
+++ sessoin10 Ex6.cpp
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
-int main() {
-    int arr[] = {9, 0, 8, 2, 0, 8};
-    int n = sizeof(arr) / sizeof(arr[0]); 
-    int key;
-    printf("Nhap phan tu can tim: ");
-    scanf("%d", &key);
-    int positions[n];
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == key) {
-            positions[count] = i + 1;
-            count++;
-        }
-    }
-
-    // Ki?m tra n?u c? ph?n t? tr?ng v?i s? d? nh?p
-    if (count > 0) {
-        printf("Phan tu %d tim thay tai cac vi tri sau: \n", key);
-        for (int i = 0; i < count; i++) {
-            printf("V? tr? %d\n", positions[i]);
-        }
-    } else {
-        printf("Phan tu %d khong ton tai trong mang.\n", key);
-    }
-
-    return 0;
+int main(){
+	int arr[100];
+	int check,gioihan,vitri,i,dem=0;
+	
+	printf("Ban muon nhap vao mang bao nhieu ki tu : ");
+	scanf("%d",&gioihan);
+	for(int i=0;i<gioihan;i++){
+		printf("Moi ban nhap gia tri : ");
+		scanf("%d",&arr[i]);
+	}
+	printf("\nMang sau khi hoan thanh : ");
+	for(int i=0;i<gioihan;i++){
+		printf("%3d",arr[i]);
+	}
+	
+	printf("\nBan muon tim gia tri nao trong mang : ");
+	scanf("%d",&check);
+	
+	for( i=0;i<gioihan;i++){
+		if(check==arr[i]){
+			vitri=i;
+			printf("Phan tu xuat hien tai vi tri %d\n",vitri);
+			++dem;
+		}
+	}
+	printf("\nPhan tu %d xuat hien %d trong mang!!!",check,dem);
+	return 0;
 }
-
